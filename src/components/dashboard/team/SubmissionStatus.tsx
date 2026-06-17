755b5922e2cbb1175d0de358e87ea328ea848c95
omsingh-blip <singhom1911in@gmail.com>
2026-06-17 19:40:19 +0530

    added few ui changes

--- src/components/dashboard/team/SubmissionStatus.tsx
+++ src/components/dashboard/team/SubmissionStatus.tsx
@@ -1,5 +1,7 @@
+import Button from "@/components/ui/Button";
 import {ProgressCard} from "@/components/ui/ProgressCard";
 import { submissionStatus } from "@/mocks/data/dashboard";
+import { Link } from "react-router-dom";
 
 export default function SubmissionStatus() {
   return (
@@ -7,6 +9,10 @@ export default function SubmissionStatus() {
       <h2 className="mb-6 text-xl font-semibold text-white">
         Submission Status
       </h2>
+
+      <Link to="/submit/hack-ai-2026">
+  <Button>Submit Project</Button>
+</Link>
 
       <div className="space-y-4">
         {submissionStatus.map((item) => (
